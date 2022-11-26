4e05762e0eedf0966bcac53e2276e4b37bec6422
f_ms <hi@imf.ms>
2022-11-27 00:33:00 +0800

    persistent

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import './App.css';
 import {MuiTheme} from "./theme/muiTheme";
-import {Box, ThemeProvider} from "@mui/material";
+import {IconButton, ThemeProvider} from "@mui/material";
 import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
 import {LocalizationProvider, zhCN} from "@mui/x-date-pickers";
 import {PublicMeAnyTypeComponent} from "./schema/MeTypes";
 import {DialogProvider} from "./component/Dialog";
+import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+import Grid2 from "@mui/material/Unstable_Grid2";
 
 function App() {
 
@@ -15,12 +18,28 @@ function App() {
         <LocalizationProvider adapterLocale={zhCN} dateAdapter={AdapterDateFns}>
             <ThemeProvider theme={MuiTheme}>
                 <DialogProvider>
-                    <Box padding={2}>
-                        <PublicMeAnyTypeComponent value={value} onChange={value => {
-                            setValue(value);
-                            console.log(value)
-                        }} />
-                    </Box>
+                    <Grid2 padding={2}>
+                        <Grid2 container justifyContent={"end"}>
+                            <IconButton onClick={() => {
+                                prompt(undefined, JSON.stringify(value))
+                            }}>
+                                <FileDownloadOutlinedIcon/>
+                            </IconButton>
+                            <IconButton onClick={() => {
+                                setValue(
+                                    JSON.parse(prompt(undefined, undefined) as any)
+                                )
+                            }}>
+                                <FileUploadOutlinedIcon/>
+                            </IconButton>
+                        </Grid2>
+                        <Grid2>
+                            <PublicMeAnyTypeComponent value={value} onChange={value => {
+                                setValue(value);
+                                console.log(value)
+                            }} />
+                        </Grid2>
+                    </Grid2>
                 </DialogProvider>
             </ThemeProvider>
         </LocalizationProvider>
